1ed860f35c68cb4678396e991d196c6357b2ab86
GitHubChrisChen8035 <13857888035@139.com>
2024-01-22 10:43:36 +0800

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -1,1 +1,1 @@
-document.getElementsByClassName("d-flex flex-wrap gap-2 mb-3")[0].getElementsByTagName('a')[3].style = "background-color:green";void(0);
+document.getElementsByClassName("d-flex flex-wrap gap-2 mb-3")[0].getElementsByTagName('a')[3].style = "background-color:white";void(0);
