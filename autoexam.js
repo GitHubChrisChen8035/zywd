6c238d3797805f7ec7aecf6ff8ac49e048668a42
GitHubChrisChen8035 <13857888035@139.com>
2024-03-19 10:11:17 +0800

    test

--- autoexam.js
+++ autoexam.js
@@ -1,24 +1,19 @@
 //auto_exam2.js
 
 var name = "在线考试";
-var filename = name + ".txt";
+var filename = "https://raw.githubusercontent.com/GitHubChrisChen8035/zywd/main/" + name + ".txt";
 console.log(filename);
 
-function fetchFileContent(filename) {
-    return fetch(filename)
-        .then(response => response.text())
-        .then(text => {
-            return text;
-        })
-        .catch(error => {
-            console.error('Error fetching file:', error);
-            return '';
-        });
-}
 
-fetchFileContent(filename).then(content => {
-    var questionsDic1 = content;
-});
+// 使用 fetch API 获取文件内容，并赋值给 var questionsDic1
+fetch(filename)
+    .then(response => response.text())
+    .then(text => {
+        var questionsDic1 = text;
+        console.log(typeof(questionsDic1),questionsDic1);
+    })
+    .catch(error => {
+        console.error('Error fetching file:', error);
+    });
 
-console.log(typeof(questionsDic1), questionsDic1);
 
