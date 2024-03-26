2a1aa0e9e1b28d5285c02a4dd9b2d046f62b1400
GitHubChrisChen8035 <13857888035@139.com>
2024-03-26 15:09:29 +0800

    Update autoexam.js

--- autoexam.js
+++ autoexam.js
@@ -14,10 +14,30 @@ if(name.slice(name.length-3) == '...'){
 }
 
 //var name = "宁波移动员工网信安全考试";
+var path = "答案集/" + name + ".txt";
 
-var filename = "https://raw.githubusercontent.com/GitHubChrisChen8035/zywd/main/答案集/" + name + ".txt";
+function getFileContent(path) {
+  const url = `https://api.github.com/repos/GitHubChrisChen8035/zywd/contents/${path}`;
 
-console.log(filename);
+  // 发送GET请求
+  fetch(url)
+    .then(response => {
+      // 检查响应状态
+      if (!response.ok) {
+        throw new Error(`本场考试暂无答案`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // 对Base64编码的内容进行解码
+      const content = decodeURIComponent(escape(atob(data.content)));
+      console.log(content);
+      return content;
+    });
+}
+
+// 调用函数获取txt答案文档内容
+var text = getFileContent(path);
 
 var questionNum = document.getElementsByClassName("list-item").length;
 console.log(questionNum);
@@ -25,43 +45,33 @@ console.log(questionNum);
 //全局定义，但不赋值，默认值将是undefined
 var questionsDic;
 
-// 使用 fetch API 获取文件内容，并赋值给questionsDic
-fetch(filename)
-    .then(response => response.text())
-    .then(text => {
-        // 正则表达式用于匹配不需要转义的双引号前后的特定字符（{, :, [, }, ,]）
-        // 并保留这些字符不变，同时转义其他情况下的双引号
-        questionsDic = JSON.parse(text.replace(/(?<![{,:[\]])"(?![,:}\]])/g, '\\"').replace(/\n/g, ""));
-	
-        console.log(typeof(questionsDic),questionsDic);
-	
-    	console.log(name + "  开始答题...");
-    	var auth = "Bearer__" + JSON.parse(localStorage.getItem("token"))["access_token"];
-    	console.log(auth);
-    	var req = new XMLHttpRequest();
-    	req.open("GET", "/api/v1/system/setting/frontend?_=" + new Date().getTime(), false);
-    	req.setRequestHeader("Authorization", auth);
-    	req.send(null);
-    	res = JSON.parse(req.responseText);
-    	currentUserId = res.currentUser.id;  
-    	
-    	var next = document.evaluate('//div[contains(@class, "border") and (text()="下一题" or text()="上一题")]', document).iterateNext();
-    	for (var i = 0; i < questionNum; i++) {
-            if (next) {
-               	try {
-        	    document.getElementsByClassName("list-item")[0].click();
-            	} catch (error) {}
-            	task(i, 0)
-            } else {
-		task(0, i)
-            }
-    	}
-    })
-    .catch(error => {
-	
-         console.log("当前考试暂无答案")
-    	 alert("当前考试暂无答案")
-    });
+// 正则表达式用于匹配不需要转义的双引号前后的特定字符（{, :, [, }, ,]）
+// 并保留这些字符不变，同时转义其他情况下的双引号
+questionsDic = JSON.parse(text.replace(/(?<![{,:[\]])"(?![,:}\]])/g, '\\"').replace(/\n/g, ""));
+
+console.log(typeof(questionsDic),questionsDic);
+
+console.log(name + "  开始答题...");
+var auth = "Bearer__" + JSON.parse(localStorage.getItem("token"))["access_token"];
+console.log(auth);
+var req = new XMLHttpRequest();
+req.open("GET", "/api/v1/system/setting/frontend?_=" + new Date().getTime(), false);
+req.setRequestHeader("Authorization", auth);
+req.send(null);
+res = JSON.parse(req.responseText);
+currentUserId = res.currentUser.id;
+
+var next = document.evaluate('//div[contains(@class, "border") and (text()="下一题" or text()="上一题")]', document).iterateNext();
+for (var i = 0; i < questionNum; i++) {
+    if (next) {
+	try {
+	    document.getElementsByClassName("list-item")[0].click();
+	} catch (error) {}
+	task(i, 0)
+    } else {
+	task(0, i)
+    }
+}
 
 //JS原生xpath选择，document.evaluate返回的是枚举类型，需要逐个取出
 function Xpath(xpath) {
